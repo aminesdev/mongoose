a6c4e3e1368c9c82e9eda8b6f0def18e1a3dd571
amine <boughabaamine76@gmail.com>
2025-09-07 22:08:04 +0100

    Basics

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 import mongoose from "mongoose";
+import { User } from "./User.js";
+
 mongoose
-    .connect("mongodb://localhost/testdb", {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then("MongoDB connected")
+    .connect("mongodb://localhost/testdb")
+    .then(() => console.log("MongoDB connected"))
     .catch((err) => console.log("Connection error:", err));
+
+async function run() {
+    const newUser = new User({ name: "Amine", age: 20 });
+    await newUser.save();
+    console.log(newUser);
+    newUser.name = "Mohamed";
+    await newUser.save();
+    console.log(newUser)
+
+}
+
+run();
